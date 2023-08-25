f33c9ec61ea1e5d048de67ec24c8927cbebae8fa
ChintanKumar Panchal <115456553+PANCHALCHINTAN@users.noreply.github.com>
2023-08-25 22:06:52 +0530

    Add files via upload
    For Math's Multiplication table

---
+++ 1ST_PROGRAMM/Multiplication_Tables.c
@@ -0,0 +1,18 @@
+//author ... CHINTANKUAMR PANCHAL
+
+#include<stdio.h>
+void main(){
+    int N;
+    printf("Enter Number:- ");
+    scanf("%d", &N);
+
+    if(N <= 0){
+        printf("You Know , Always Your Answere will 0..... :) \n");
+    }
+
+    //Main Logic to print mulplication tables
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d X %d = %d\n", N, i, N * i);
+    }
+}
